4f9b89438144033fa34fcb9ad95ab0f97bbfeaac
jbosquez012 <121687390+jbosquez012@users.noreply.github.com>
2023-04-04 20:00:42 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-h1ph2s
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-h1ph2s)
---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./style.css";
+
+export default function App() {
+  return (
+    <div>
+      <h1>Lesson 6 HandsOn
+        
+      </h1>
+      <p>Start editing to see some magic happen :)</p>
+    </div>
+  );
+}
